698c8a02e675eab4a5807f8f811efecedcbceb2d
Chuchamonga <124098995+Chuchamonga@users.noreply.github.com>
2025-03-21 18:38:39 -0400

    Update anim.js

--- anim.js
+++ anim.js
@@ -81,8 +81,8 @@ function updateLyrics() {
 
   if (currentLine) {
     // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
-    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+    var fadeInDuration = 0.2; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time-1) / fadeInDuration);
 
     // Aplica el efecto de aparición
     lyrics.style.opacity = opacity;
